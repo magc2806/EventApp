cea375b8cc6db7885324070b60ed7e61fb7093cf
magc2806 <gonzalezcabellomanuel@gmail.com>
2024-08-15 19:42:46 -0300

    Add Basic landing (#2)
    * chore(tailwind): configure custom colors
    
    feat(landing-page): add initial version of landing page
    
    * refactor(controller): rename WelcomeController to PagesController
    
    refactor(test): adapt tests to new controller and action name

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#791BCC',       // Main color
+        accent: '#DB1F18',        // Accent   color
+        neutral: '#B2ADB8',       // Neutral color
+        surface: '#F5F5F5',
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
